dc4af491a50996b53a7079ec398c3425a12d75ef
hehh <lingtinzhe@icloud.com>
2018-12-29 10:00:18 +0800

    删除错误代码

--- src/main/java/cn/listenerhe/TestController.java
+++ src/main/java/cn/listenerhe/TestController.java
@@ -2,12 +2,9 @@ package cn.listenerhe;
 
 import cn.listenerhe.core.annotation.ControllerKey;
 import cn.listenerhe.core.annotation.CrossOrigin;
-import cn.listenerhe.core.annotation.RequestBody;
 import cn.listenerhe.core.annotation.validation.NotBlank;
-import cn.listenerhe.core.annotation.validation.Size;
 import cn.listenerhe.core.annotation.validation.Validated;
-import cn.listenerhe.core.sql.BaseDb;
-import cn.listenerhe.core.sql.ModelExampleSql;
+import cn.listenerhe.core.sql.ModelExample;
 import cn.listenerhe.model.Poems;
 import com.jfinal.aop.Inject;
 import com.jfinal.core.Controller;
@@ -27,8 +24,8 @@ public class TestController extends Controller{
     @Validated
     @CrossOrigin
     public void index(@NotBlank(msg = "不能为空啊!!!")  String name){
-        ModelExampleSql modelExampleSql = new ModelExampleSql(Poems.class);
-        ModelExampleSql.Criteria criteria = modelExampleSql.createCriteria();
+        ModelExample modelExampleSql = new ModelExample(Poems.class);
+        ModelExample.Criteria criteria = modelExampleSql.createCriteria();
         criteria.andLike("author","%和%");
         renderJson(poemsBaseDb.pageByExample(1,5,modelExampleSql));
     }
